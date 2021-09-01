be8782631dd73cb24f3b6495f8d09051099cc2b0
nsakki55 <n.sakki55@gmail.com>
2021-09-01 10:46:23 +0900

    add pytest mock args

--- pytest_mock_arg_test/src/func.py
+++ pytest_mock_arg_test/src/func.py
@@ -13,9 +13,9 @@ def process_one(arg1: List[float], arg2: List[float]) -> float:
     return sum_of_args
 
 
-# def process_two(arg1: List[float], arg2: List[float], arg3:List[float], arg4:List[float]) -> (float, float):
-#     """Use calc_sum function twice in process"""
-#     sum_of_args = calc_sum(arg1=arg1, arg2=arg2)
-#     sum_of_args2 = calc_sum(arg1=arg3, arg2=arg4)
-#
-#     return sum_of_args, sum_of_args2
+def process_two(arg1: List[float], arg2: List[float], arg3:List[float], arg4:List[float]) -> (float, float):
+    """Use calc_sum function twice in process"""
+    sum_of_args = calc_sum(arg1=arg1, arg2=arg2)
+    sum_of_args2 = calc_sum(arg1=arg3, arg2=arg4)
+
+    return sum_of_args, sum_of_args2
--- pytest_mock_arg_test/test/test_func.py
+++ pytest_mock_arg_test/test/test_func.py
@@ -1,6 +1,6 @@
 from unittest.mock import MagicMock, call, patch
 
-from src.func import process_one
+from src.func import process_one, process_two
 
 
 @patch("src.func.calc_sum")
@@ -14,13 +14,11 @@ def test_process_one(mocked_calc_sum: MagicMock):
     print(actual_args[1])
 
 
-#
-# @patch("src.func.calc_sum")
-# def test_process_two(mocked_calc_sum: MagicMock):
-#     actual = process_two(arg1=[0.1, 0.2], arg2=[0.3, 0.4], arg3=[0.5, 0.6], arg4=[0.7, 0.8])
-#     actual_args = mocked_calc_sum.call_args_list
-#     print(actual_args)
-#     print(type(actual_args[0]))
-#     print(actual_args[0][1])
-#     print(type(actual_args[1]))
-#     print(actual_args[1])
+
+@patch("src.func.calc_sum")
+def test_process_two(mocked_calc_sum: MagicMock):
+    actual = process_two(arg1=[0.1, 0.2], arg2=[0.3, 0.4], arg3=[0.5, 0.6], arg4=[0.7, 0.8])
+    actual_args = mocked_calc_sum.call_args_list
+    print(actual_args)
+    print(actual_args[0][1])
+    print(actual_args[1][1])
